1ee1782712ea70fb4faff019fae2df7922d6983e
Andres Cagua <af.cagua1011@gmail.com>
2019-06-02 02:17:25 -0500

    ajuste correo proveedores email

--- server/services/mail/mail.js
+++ server/services/mail/mail.js
@@ -1,9 +1,12 @@
 var nodemailer = require("nodemailer");
 var fs = require('fs');
 
-//templates
-var templateNewOrder = fs.readFileSync('server/services/mail/templates/newOrder.html', { encoding: 'utf-8' });
-var templateNewOrderStore = fs.readFileSync('server/services/mail/templates/newOrderStore.html', { encoding: 'utf-8' });
+//templates desarrollo
+// var templateNewOrder = fs.readFileSync('server/services/mail/templates/newOrder.html', { encoding: 'utf-8' });
+// var templateNewOrderStore = fs.readFileSync('server/services/mail/templates/newOrderStore.html', { encoding: 'utf-8' });
+//templates produccion
+var templateNewOrder = fs.readFileSync('/var/www/camaleon.shop/server/services/mail/templates/newOrder.html', { encoding: 'utf-8' });
+var templateNewOrderStore = fs.readFileSync('/var/www/camaleon.shop/server/services/mail/templates/newOrderStore.html', { encoding: 'utf-8' });
 
 var mailFrom = 'info@camaleon.shop';
 
@@ -17,10 +20,8 @@ var transporter = nodemailer.createTransport({
     }
 });
 
-exports.sendOrderMail = function (parameters)
-{
-    try
-    {
+exports.sendOrderMail = function(parameters) {
+    try {
         var order = parameters.order;
 
         var bcc = ['info@camaleon.shop', 'nicolaz@camaleon.shop'];
@@ -36,14 +37,10 @@ exports.sendOrderMail = function (parameters)
             html: templateNewOrder
         };
 
-        transporter.sendMail(mailOptions, function (error, info)
-        {
-            if (error)
-            {
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
                 console.log(error);
-            }
-            else
-            {
+            } else {
                 console.log("Email sent");
             }
         });
@@ -51,15 +48,12 @@ exports.sendOrderMail = function (parameters)
         //2. mail to the stores
         console.log('b:::parameters.stores: ' + JSON.stringify(parameters.stores) + '\n');
 
-        parameters.stores.forEach(element1 =>
-        {
+        parameters.stores.forEach(element1 => {
             var htmlTemplate = templateNewOrderStore;
             var htmlOrderTable = '<table>';
             // replace order table
-            order.items.forEach(element2 =>
-            {
-                if (element2.item._storeId + 'x' === element1._id + 'x')
-                {
+            order.items.forEach(element2 => {
+                if (element2.item._storeId + 'x' === element1._id + 'x') {
                     htmlOrderTable += '<tr>';
                     htmlOrderTable += ' <th>Producto</th>';
                     htmlOrderTable += ' <th>Cantidad</th>';
@@ -85,36 +79,16 @@ exports.sendOrderMail = function (parameters)
                 html: htmlTemplate
             };
 
-            transporter.sendMail(mailOptions, function (error, info)
-            {
-                if (error)
-                {
+            transporter.sendMail(mailOptions, function(error, info) {
+                if (error) {
                     console.log(error);
-                }
-                else
-                {
+                } else {
                     console.log("Email sent");
                 }
             });
         });
 
-    }
-    catch (error)
-    {
+    } catch (error) {
         console.log('error en mail - sendOrderMail: ' + error);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
